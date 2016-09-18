ec3a3f79868179543c0cbfd6d08d4c27ebc848c4
forrestsill <forrestsill@gmail.com>
2016-09-18 09:03:15 -0400

    Updated dict parsing to be Python3-compatible)

--- get-tweets.py
+++ get-tweets.py
@@ -1,4 +1,4 @@
 import requests, json, random
 url = 'https://hackthenorth-b9b85.firebaseio.com/negtweets.json?auth=k8oUcZFSAcbBZpHcQkyTPeepFj8ckSG4zQcp1tme&print=pretty'
 response = requests.get(url)
-json.loads(response.text)[random.choice(json.loads(response.text).keys())]['message']
+json.loads(response.text)[random.choice(list(json.loads(response.text).keys()))]['message']
