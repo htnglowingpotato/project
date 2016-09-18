0ce8ebe442733330ee1ab3255739b6fe1a778294
forrestsill <forrestsill@gmail.com>
2016-09-18 08:26:00 -0400

    Added get_tweet method to game

--- bullshit-breaker.py
+++ bullshit-breaker.py
@@ -1,4 +1,4 @@
-import sys, pygame
+import sys, pygame, requests, json, random
 from brick import Brick
 from brick import Player
 
@@ -58,6 +58,11 @@ ballrect.y = 400
 #string to text
 
 test_string1 = "Never met but never liked dopey Robert Gates. Look at the mess the U.S. is in. Always speaks badly of his many bosses, including Obama."
+
+def get_tweet:
+	url = 'https://hackthenorth-b9b85.firebaseio.com/negtweets.json?auth=k8oUcZFSAcbBZpHcQkyTPeepFj8ckSG4zQcp1tme&print=pretty'
+	response = requests.get(url)
+	return json.loads(response.text)[random.choice(json.loads(response.text).keys())]['message']
 
 #string to text
 block_words = []
