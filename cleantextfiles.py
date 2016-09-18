dc9055571d1bc200857bf6c0da3aacc9e2503def
juliettehainline <juliette.hainline@gmail.com>
2016-09-18 01:56:49 -0400

    committing the basic naivebayes

--- cleantextfiles.py
+++ cleantextfiles.py
@@ -1,11 +1,7 @@
-import collections
-import nltk
-import nltk.classify.util, nltk.metrics
-from nltk.classify import NaiveBayesClassifier
+import random
+from textblob.classifiers import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
-from nltk.probability import FreqDist, ELEProbDist
-from nltk.classify.util import apply_features,accuracy
-#
+
 # Thank you http://stackoverflow.com/questions/14663428/python-cleaning-words-in-a-sentence
 #Cleaning Sentence
 def remove_unw2anted(str):
@@ -42,62 +38,40 @@ i = 0
 pos = []
 bs = []
 while i<len(cleanpos):
-    pos.append("positive")
+    pos.append("pos")
     i+=1
 pos_tweets = list(zip(cleanpos, pos))
 # print (newpos)
 i = 0
 while i<len(cleanbs):
-    bs.append("negative")
+    bs.append("neg")
     i+=1
 neg_tweets = list(zip(cleanbs, bs))
 
-tweets = []
+train = []
 for (words, sentiment) in pos_tweets + neg_tweets:
     words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
-    tweets.append((words_filtered, sentiment))
+    train.append((words_filtered, sentiment))
 
-test_tweets = [
-(['feel', 'happy', 'this', 'morning'], 'positive'),
-(['larry', 'friend'], 'positive'),
-(['not', 'like', 'that', 'man'], 'negative'),
-(['house', 'not', 'great'], 'negative'),
-(['your', 'song', 'annoying'], 'negative')]
+test = [
+(['feel', 'happy', 'this', 'morning'], 'pos'),
+(['larry', 'friend'], 'pos'),
+(['not', 'like', 'that', 'man'], 'neg'),
+(['house', 'not', 'great'], 'neg'),
+(['your', 'song', 'annoying'], 'neg')]
 
-def get_word_features(wordlist):
-    wordlist = nltk.FreqDist(wordlist)
-    word_features = wordlist.keys()
-    return word_features
+cl = NaiveBayesClassifier(train)
 
-def get_words_in_tweets(tweets):
-    all_words = []
-    for (words, sentiment) in tweets:
-      all_words.extend(words)
-    return all_words
+reviews = [(list(movie_reviews.words(fileid)), category)
+  for category in movie_reviews.categories()
+  for fileid in movie_reviews.fileids(category)]
+new_train, new_test = reviews[0:100], reviews[101:200]
+cl.update(new_train)
+accuracy = cl.accuracy(test + new_test)
+# print(cl.classify("Their burgers are amazing"))
+# print(cl.classify("But the hangover was horrible. My boss was not happy."))
+# print(cl.classify("There is a guy who has got nothing going, a waste!"))
+# print(cl.accuracy(test))
 
-word_features = get_word_features(get_words_in_tweets(tweets))
-print (word_features)
-
-def extract_features(document):
-    document_words = set(document)
-    features = {}
-    for word in word_features:
-        features['contains(%s)' % word] = (word in document_words)
-    return features
-
-# training_set = nltk.classify.apply_features(extract_features, tweets)
-#
-# def classifier(training_set):
-#     return nltk.NaiveBayesClassifier.train(training_set)
-#
-# def train(labeled_featuresets, estimator=ELEProbDist):
-#     # Create the P(label) distribution
-#     label_probdist = estimator(label_freqdist)
-#     ...
-#     # Create the P(fval|label, fname) distribution
-#     feature_probdist = {}
-#     ...
-#     return NaiveBayesClassifier(label_probdist, feature_probdist)
-#
-# def classify(tweet):
-#     print classifier(classify(extract_features(tweet.split()))
+def posorneg(tweet):
+    return cl.classify(tweet)
